c641fdba9955103bc0d9ee66c04312e9cd847644
Águeda Gambón <87611812+gambona@users.noreply.github.com>
2024-09-09 23:16:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ data = {
         31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
         41, 42, 43, 44, 45, 46, 47, 48, 49, 50
     ],
-    "Deliverable": [
+    "Deliverables": [
         "YouTube Video", "Sponsored Content", "Instagram Post", "Podcast Promotion", "Blog Review", 
         "Sponsored Content", "Sponsored Content", "YouTube Video", "Instagram Post", "Instagram Post", 
         "Sponsored Content", "Blog Review", "Instagram Post", "Podcast Promotion", "YouTube Video", 
