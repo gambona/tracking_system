4b3ac1cf231ca27915d2e1e25ed3a6fa3392a38b
Águeda Gambón <87611812+gambona@users.noreply.github.com>
2024-09-09 23:43:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="My App", theme="dark")
 st.set_page_config(
     page_title="8 Sleep Affiliates & Influencers Tracking System",
     page_icon="📘",
