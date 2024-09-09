ea9562963fcd1531ef92eaad11cf7c42a94f20e2
Águeda Gambón <87611812+gambona@users.noreply.github.com>
2024-09-09 22:36:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def mostrar_partners():
 
 def mostrar_contract ():
 
-    st.subheader("Update Contracts")
+    st.subheader("Contracts")
     contract_to_update = st.number_input("Enter Contract ID to Update", min_value=1, step=1)
     new_status = st.selectbox("New Status", ["Not Started", "In Progress", "Completed"], key="update_status")
 
